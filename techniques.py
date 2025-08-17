28e7233fc60313015ca7b823894b8f1b72f0bdc0
DamECP <damien.depaepe@gmail.com>
2025-08-17 18:16:36 +0200

    refactor: unify cell group methods with a dict

--- techniques.py
+++ techniques.py
@@ -1,9 +1,10 @@
 from classes import Sudoku, Cell
+from itertools import combinations
 
 
 def repeat_until_stable(technique):
     """
-    Decorator : applies a technique until there are no changes
+    Decorator : applies a technique until there is no progress
     """
 
     def wrapper(sudoku):
@@ -66,14 +67,53 @@ def naked_pairs_triples(sudoku):
             sudoku.get_square(cell.square),
         ]
 
+        # Same process for row/col/square
         for i, group in enumerate(group_candidates):
+
+            # if {1,3,4} are the only candidates for 3 cells
             if identical_candidates_number[i] > 1 and identical_candidates_number[
                 i
             ] == len(ref):
                 for c in row_col_square_cells[i]:
                     if c.candidates != ref:
                         for value in ref:
+                            # take those values off the other cells'candidates
                             if c.discard_candidate(value):
+                                progress = True
+
+    return progress
+
+
+def hidden_pairs_triples(sudoku):
+
+    progress = True
+
+    while progress:
+        progress = False
+
+        for cell in sudoku.cells.values():
+            if cell.value is not None:
+                continue
+
+            ref = cell.candidates
+
+            # Builds all possible combination : pairs, then triples
+            for combi_length in range(2, 4):
+                combi = combinations(ref, combi_length)
+
+                found = []
+                not_found = []
+
+                for compared_cell in sudoku.get_row_candidates(cell.row):
+                    if compared_cell.candidates.issupperset(combi):
+                        found.append(compared_cell)
+                    else:
+                        not_found.append(compared_cell)
+
+                if len(found) == combi_length:
+                    for value in combi:
+                        for c in not_found:
+                            if c.discard(value):
                                 progress = True
 
     return progress
